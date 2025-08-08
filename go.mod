d9625e8803dd2eb040b1246b913e0d3ca4b26a9a
ilia.babushkin <ilia.babushkin@stdevteam.com>
2025-08-08 16:10:05 +0200

    add hjson library for parsing and encoding Hjson data

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/idib/got2
 
 go 1.24.5
 
-require github.com/idib/got1 v1.1.1
+require (
+	github.com/hjson/hjson-go/v4 v4.5.0
+	github.com/idib/got1 v1.1.1
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/hjson/hjson-go/v4"
 	"github.com/idib/got2/pkg/client"
 )
 
@@ -35,4 +36,45 @@ func main() {
 
 		time.Sleep(1 * time.Second)
 	}
+	sampleText := []byte(`
+    {
+        # specify rate in requests/second
+        rate: 1000
+        array:
+        [
+            foo
+            bar
+        ]
+    }`)
+
+	// We need to provide a variable where Hjson
+	// can put the decoded data.
+	var dat map[string]interface{}
+
+	// Decode with default options and check for errors.
+	if err := hjson.Unmarshal(sampleText, &dat); err != nil {
+		panic(err)
+	}
+	// short for:
+	// options := hjson.DefaultDecoderOptions()
+	// err := hjson.UnmarshalWithOptions(sampleText, &dat, options)
+	fmt.Println(dat)
+
+	// In order to use the values in the decoded map,
+	// we'll need to cast them to their appropriate type.
+
+	rate := dat["rate"].(float64)
+	fmt.Println(rate)
+
+	array := dat["array"].([]interface{})
+	str1 := array[0].(string)
+	fmt.Println(str1)
+
+	// To encode to Hjson with default options:
+	sampleMap := map[string]int{"apple": 5, "lettuce": 7}
+	hjson, _ := hjson.Marshal(sampleMap)
+	// short for:
+	// options := hjson.DefaultOptions()
+	// hjson, _ := hjson.MarshalWithOptions(sampleMap, options)
+	fmt.Println(string(hjson))
 }
